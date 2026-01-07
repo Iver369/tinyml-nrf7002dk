25f511c84df7a8f334e641d331eb8324105c4939
Iver369 <227830548+Iver369@users.noreply.github.com>
2026-01-07 14:55:48 +0100

    99.67% reduction in inference time + WiFi configuration

--- firmware/src/dsp.c
+++ firmware/src/dsp.c
@@ -1,30 +1,73 @@
 #include "dsp.h"
-#include <arm_math.h> 
-#include <zephyr/sys/printk.h>
+#include "data.h"          
+#include "mel_constants.h" 
 
-// matching train_kws parameters
-#define FRAME_LEN 256
-#define FRAME_STEP 240
-#define NUM_FRAMES 66
-#define FFT_SIZE 256
+#include <zephyr/kernel.h>
+#include <arm_math.h>      
+#include <math.h>          
 
-int generate_spectrogram(const int16_t* waveform, float* spectrogram) {
-static float frame_buffer[FRAME_LEN] __attribute__((aligned(16)));
-static float fft_output[FFT_SIZE + 2] __attribute__((aligned(16)));
+// aliogned(16) for SIMD instructions
+// matching python training configuration
+static float fft_input_buffer[FFT_SIZE] __attribute__((aligned(16)));
+static float fft_output_buffer[FFT_SIZE] __attribute__((aligned(16)));
+static float fft_magnitude_buffer[NUM_FFT_BINS] __attribute__((aligned(16)));
+
+static float window_func[FFT_SIZE] __attribute__((aligned(16)));
 static arm_rfft_fast_instance_f32 fft_instance;
-    arm_rfft_fast_init_f32(&fft_instance, FFT_SIZE);
-    for (int i = 0; i < NUM_FRAMES; i++) {
-        for (int j = 0; j < FRAME_LEN; j++) {
-            frame_buffer[j] = (float)waveform[i * FRAME_STEP + j] / 32768.0f;
+static bool is_initialized = false;
+
+void dsp_init(void) {
+    if (!is_initialized) {
+        arm_rfft_fast_init_f32(&fft_instance, FFT_SIZE);
+        for (int i =0; i < FFT_SIZE; i++) {
+            window_func[i] = 0.5f - 0.5f *arm_cos_f32(2.0f * PI * (float)i / (float)FFT_SIZE);
         }
 
-        arm_rfft_fast_f32(&fft_instance, frame_buffer, fft_output, 0);
-
-        for (int j = 0; j < (FFT_SIZE / 2) + 1; j++) {
-            float real = fft_output[j * 2];
-            float imag = fft_output[j * 2 + 1];
-            spectrogram[i * 129 + j] = sqrtf(real * real + imag * imag);
-        }
+        is_initialized = true;
     }
+}
+
+int generate_spectrogram(const int16_t* audio_in, float* features_out) {
+    if (!is_initialized) dsp_init();
+
+    int read_index = 0; 
+    int write_index = 0; 
+
+    for (int i = 0; i < SPECTROGRAM_ROWS; i++) {
+        for (int j = 0; j < FFT_SIZE; j++) {
+            if (read_index + j < NUM_SAMPLES) {
+                // dividing by 32768 for neural network to work with ranges between -1 and 1
+                float normalized = (float)audio_in[read_index + j] / 32768.0f;
+                fft_input_buffer[j] = normalized * window_func[j]; // smoothing edges to avoid spectral leakage
+            } else {
+                fft_input_buffer[j] = 0.0f;
+            }
+        }
+        /*
+        the output is symmetrical because the input is real numbers (audio)
+        so arm_rfft_fast_f32 deletes second half and then the dc and nyquist frequencies
+        gets packed as they are purely real numbers
+        */ 
+        arm_rfft_fast_f32(&fft_instance, fft_input_buffer, fft_output_buffer, 0);
+
+        float dc = fft_output_buffer[0];
+        fft_magnitude_buffer[0] = (dc>= 0) ? dc : -dc; 
+        float nyquist = fft_output_buffer[1];
+        fft_magnitude_buffer[NUM_FFT_BINS - 1] = (nyquist >= 0) ? nyquist : -nyquist; 
+        arm_cmplx_mag_f32(fft_output_buffer +2, fft_magnitude_buffer + 1, NUM_FFT_BINS - 2);
+
+        for (int m = 0; m < NUM_MEL_BINS; m++) {
+            float energy_sum = 0.0f;
+            
+            for (int k = 0; k < NUM_FFT_BINS; k++) {
+                energy_sum += fft_magnitude_buffer[k] * mel_filterbank[k][m];
+            }
+            // adding 1e-6f to prevent log(0)
+            features_out[write_index++] = logf(energy_sum + 1e-6f);
+        }
+
+        read_index += WINDOW_STEP;
+    }
+
     return 0;
 }
--- firmware/src/inference.cpp
+++ firmware/src/inference.cpp
@@ -1,29 +1,30 @@
 #include "inference.h"
 #include <zephyr/kernel.h>
 #include <zephyr/sys/printk.h>
+
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/system_setup.h"
 #include "tensorflow/lite/schema/schema_generated.h"
-#include "model.h"
-
+#include "model.h" 
 
 const tflite::Model* model = nullptr;
 tflite::MicroInterpreter* interpreter = nullptr;
-const int kTensorArenaSize = 200 * 1024; 
-static uint8_t tensor_arena[kTensorArenaSize] __attribute__((aligned(16)));
+//statically allocate memory, so that it doesn't crash
+const int kTensorArenaSize = 165 * 1024; // 165 as AllocateTensors() kept failing for having too low buffer
+static uint8_t tensor_arena[kTensorArenaSize] __attribute__((aligned(16))); 
 
-TfLiteTensor* input = nullptr;
-TfLiteTensor* output = nullptr;
+static TfLiteTensor* model_input = nullptr;
+static TfLiteTensor* model_output = nullptr;
 
 extern "C" int inference_setup(void) {
-    model = tflite::GetModel(model_universal);
+    model = tflite::GetModel(g_model);
     if (model->version() != TFLITE_SCHEMA_VERSION) {
-        printk("Model schema version mismatch\n");
+        printk("Model schema version mismatch\n"); // checks if model trained in python is compatible with the tflm 
         return -1;
     }
 
-    static tflite::MicroMutableOpResolver<15> micro_op_resolver;
+    static tflite::MicroMutableOpResolver<15> micro_op_resolver; // using specific math functions to save memory rather than including all 
     micro_op_resolver.AddConv2D();
     micro_op_resolver.AddDepthwiseConv2D();
     micro_op_resolver.AddMaxPool2D();
@@ -38,33 +39,42 @@ extern "C" int inference_setup(void) {
     micro_op_resolver.AddDequantize();
 
     static tflite::MicroInterpreter static_interpreter(
-    model, micro_op_resolver, tensor_arena, kTensorArenaSize);
+        model, micro_op_resolver, tensor_arena, kTensorArenaSize);
     interpreter = &static_interpreter;
-
+    
     if (interpreter->AllocateTensors() != kTfLiteOk) {
-        printk("AllocateTensors() failed\n");
+        printk("Arena too small for this model architecture\n");
         return -1;
     }
 
-    input = interpreter->input(0);
-    output = interpreter->output(0);
+    model_input = interpreter->input(0);
+    model_output = interpreter->output(0);
 
+    if (model_input == nullptr || model_output == nullptr) {
+        printk("Tensors are NULL\n");
+        return -1; // stops program as it would crash
+    }
+    
     return 0;
 }
 
 extern "C" int run_inference(float* spectrogram_data) {
-    if (!interpreter) return -1;
-    for (int i = 0; i < (66 * 129); i++) {
+    if (model_input == nullptr || model_output == nullptr) return -1;
+    int num_elements = model_input->bytes; 
+    // quantization as the dsp speaks in floats 32 bit, and the ai model 8-bit integers
+    // mapping numbers
+    for (int i = 0; i <num_elements; i++) {
         float val = spectrogram_data[i];
-        int8_t q_val = (int8_t)(val / input->params.scale + input->params.zero_point);
-        input->data.int8[i] = q_val;
+        int8_t q_val = (int8_t)(val /model_input->params.scale + model_input->params.zero_point);
+        model_input->data.int8[i] = q_val;
     }
 
     if (interpreter->Invoke() != kTfLiteOk) {
-        printk("Invoke failed\n");
+        printk("Interpreter Invoke failed\n"); // check to see if the math ran correctly 
         return -1;
     }
-    float probability = (output->data.int8[2] - output->params.zero_point) * output->params.scale;
-    
+    // dequantization, outputs an integer and converts back to float for probability
+    float probability = (model_output->data.int8[2] - model_output->params.zero_point) * model_output->params.scale;
+    // return probability so that main can handle it
     return (int)(probability * 100.0f);
 }
